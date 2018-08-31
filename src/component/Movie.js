9b74a6ef0b7bca9bfe0a99d82ce67bc17ec6ee81
xsiasu <xsiasu@gmail.com>
2018-08-31 19:06:02 +0900

    task

--- src/component/Movie.js
+++ src/component/Movie.js
@@ -1,20 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types'; // 라이브러리
-
+import LinesEllipsis from 'react-lines-ellipsis' //라이브러리
 
 // 멍청한 함수 만듬 리턴하지 못하고 렌더도 없음
 function Movie({title,poster,genres,synopsis}) {
 	return(
-		<div>
-			<h1>{title}</h1>
-			<div>
-			
-			<MoviePoster poster={poster} title={title}/>	
+		<div className="Movie">
+			<div className="Movie__Colum">
+				<div>
+					<MoviePoster poster={poster} title={title}/>	
+				</div>
 			</div>
-			<div>
-				{genres.map((genre,index) => <MovieGenres genre={genre} key={index} />)}
+			<div className="Movie__Colum">
+				<h1>{title}</h1>
+				<div className="Movie__Genres">
+					{genres.map((genre,index) => <MovieGenres genre={genre} key={index} />)}
+				</div>
+				<div className="Movie__Synopsis">
+					<LinesEllipsis
+					text={synopsis}
+					maxLine='3'
+					ellipsis='...'
+					trimRight
+					basedOn='letters'
+					/> 				
+				</div>
 			</div>
-			<div>{synopsis}</div>
 		</div>
 	)
 }
@@ -31,9 +42,10 @@ function MoviePoster({poster,title}) {
 
 function MovieGenres({genre}) {
 	return(
-		<span>{genre}</span>
+		<span className="Movie__Genre">{genre}</span>
 	)
 }
+
   /********************************************* 
    * genres 는 array 인데 그것도 모르고 계속 스트링으로 받아서 에러 계속 남
   *********************************************/	
@@ -44,13 +56,6 @@ Movie.propTypes = {
 	synopsis : PropTypes.string.isRequired
 }
 
-// Movie.propTypes = {
-//     title: PropTypes.string.isRequired,
-//     poster: PropTypes.string.isRequired,
-//     genres: PropTypes.array.isRequired,
-//     synopsis: PropTypes.string.isRequired
-// }
-
 MoviePoster.propTypes = {
 	poster : PropTypes.string.isRequired
 }	
@@ -58,7 +63,5 @@ MoviePoster.propTypes = {
 MovieGenres.propTypes = {
 	genre : PropTypes.string.isRequired
 }
-
-
 
 export default Movie;
